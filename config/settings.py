db0dd160e7bd44bd9d22a59103184485296c39dd
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-10-02 22:21:37 +0330

    section-9

--- config/settings.py
+++ config/settings.py
@@ -134,7 +134,7 @@ REST_FRAMEWORK = {
         'api.permissions.IsStaffOrReadOnly',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
