b300e77ce33fbec5d9d807ef41f6c1275fd9bf22
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-10-18 21:14:57 +0330

    section-24

--- config/settings.py
+++ config/settings.py
@@ -151,7 +151,10 @@ REST_FRAMEWORK = {
         # 'rest_framework_simplejwt.authentication.JWTAuthentication',
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
     ],
-     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+     'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+     ]
 }
 
 REST_USE_JWT = True
