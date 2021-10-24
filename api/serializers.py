09bc591341dc16a5dceeb216d0dffc7e2397cf36
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-10-24 20:59:41 +0330

    section-28

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,16 @@ from rest_framework import serializers
 from blog.models import Articles
 from django.contrib.auth import get_user_model
 
+class AuthorSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = get_user_model()
+		fields = ["id", "username", "first_name", "last_name"]
+
+
+
 class ArticleSerializer(serializers.ModelSerializer):
+	author = AuthorSerializer()
+
 	class Meta:
 		model = Articles
 		# fields = ("title", "slug", "author", "content", "publish", "status", )
